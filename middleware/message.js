e39041fbb20c71e1646b8eac04b414952a4206c0
Vinicius Pietscher <8095975+viniciuspietscher@users.noreply.github.com>
2022-09-07 23:17:58 -0400

    refactor to ternary

--- middleware/message.js
+++ middleware/message.js
@@ -1,9 +1,7 @@
 const setMessage = (req, res, next) => {
-  if (req.session.pendingMessage) {
-    res.locals.message = req.session.pendingMessage
-  } else {
-    res.locals.message = ""
-  }
+  req.session.pendingMessage
+    ? (res.locals.message = req.session.pendingMessage)
+    : (res.locals.message = "")
   req.session.pendingMessage = ""
   next()
 }
